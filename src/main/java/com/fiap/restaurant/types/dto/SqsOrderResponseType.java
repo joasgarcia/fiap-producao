3a3cb026cdc6133721e9fb65f1102c95194188db
Joás Garcia <joasdemelo@hotmail.com>
2024-03-29 10:16:41 -0300

    Corrigindo envio para tornar compatível com o consumo do serviço de pedidos

--- src/main/java/com/fiap/restaurant/types/dto/SqsOrderResponseType.java
+++
@@ -1,6 +0,0 @@
-package com.fiap.restaurant.types.dto;
-
-public enum SqsOrderResponseType {
-
-    ORDER_PRODUCTION_FINISHED
-}
---
+++ src/main/java/com/fiap/restaurant/types/dto/services/ServiceResponseQueueType.java
@@ -0,0 +1,5 @@
+package com.fiap.restaurant.types.dto.services;
+
+public enum ServiceResponseQueueType {
+    ORDER_PRODUCTION_FINISHED
+}
